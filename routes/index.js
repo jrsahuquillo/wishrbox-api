e9fc6099bdef94a8050560db13d363e4b118ada2
jrsahuquillo <jrsahuquillo@gmail.com>
2017-07-01 19:27:14 +0200

    Auth middlewares

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const wishCrtl = require('../controllers/wish')
+const auth = require('..middlewares/auth')
 const api = express.Router()
 
 api.get('/wish', wishCrtl.getWishes)
@@ -9,5 +10,8 @@ api.get('/wish/:wishId', wishCrtl.getWish)
 api.post('/wish', wishCrtl.saveWish)
 api.put('/wish/:wishId', wishCrtl.updateWish)
 api.delete('/wish/:wishId', wishCrtl.deleteWish)
+api.get('/private', auth.isAuth, function(req, res) {
+  res.status(200).send({ message: `Access accepted` })
+})
 
 module.exports = api
